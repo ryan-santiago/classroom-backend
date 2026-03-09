cfc670d12b4da0fa7afc283cbf0edd76fb3c9d83
Ryan Santiago <ryan.santiago.ict@gmail.com>
2026-03-09 23:29:49 +0800

    git save

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/neon-http'
 import { neon } from '@neondatabase/serverless'
 
 if (!process.env.DATABASE_URL) {
-	throw new Error('DATABASE_URL is not defined test commit')
+	throw new Error('DATABASE_URL is not defined')
 }
 
 const sql = neon(process.env.DATABASE_URL)
