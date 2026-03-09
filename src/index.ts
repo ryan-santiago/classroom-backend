7df33396fea355f998c766553999584038115429
Ryan Santiago <ryan.santiago.ict@gmail.com>
2026-03-09 23:16:28 +0800

    implement GET all subjects and cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import express from 'express'
+import cors from 'cors'
+import subjectsRouter from './routes/subjects'
 
 const app = express()
 const PORT = 8000
 
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL,
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		credentials: true,
+	}),
+)
+
 // middleware
 app.use(express.json())
 
+app.use('/api/subjects', subjectsRouter)
 // routes
 app.get('/', (_req, res) => {
 	res.json({ message: 'Hello from classroom-backend!' })
